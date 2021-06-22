444d3fb233ccbb225c60557297aea1237f6aa31d
nadezdr <62659940+nadezdr@users.noreply.github.com>
2021-06-22 13:01:26 +0200

    Add files via upload

---
+++ Projekat-blok-graf-main/Projekat blok graf/Grana.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_blok_graf
+{
+    class Grana
+    {
+        //int tezina;
+        Cvor Od;
+        Cvor Do;
+        Color boja;
+
+        public Color Boja
+        {
+            get { return boja; }
+            set { boja = value; }
+        } 
+
+        public Grana(Cvor Od, Cvor Do)
+        {
+            this.Od = Od;
+            this.Do = Do;
+        }
+    }
+}
